0cc0570b3db9a456d2a51baa082e1e08b929f858
tetetratra <44367208+tetetratra@users.noreply.github.com>
2025-06-06 08:06:25 +0900

    Fix lazy.nvim module path

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,7 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("lua.plugins")
 
 require("lua.utils")
 require("lua.options")
