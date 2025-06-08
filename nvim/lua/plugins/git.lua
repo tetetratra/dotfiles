553dc7f42539f38ed745ab780d31647e1d41fc6e
tetetratra <44367208+tetetratra@users.noreply.github.com>
2025-06-09 00:10:24 +0900

    手動lint

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,7 +2,6 @@
 return {
   {
     'tpope/vim-fugitive',
-    cmd = { 'Git', 'Ggrep', 'GBrowse' },
     config = function()
       vim.cmd('cabbrev g Ggrep')
       vim.cmd('cabbrev l Git log --follow -p %')
@@ -13,16 +12,16 @@ return {
     'airblade/vim-gitgutter',
     config = function()
       vim.cmd([[
-    let g:gitgutter_git_executable = substitute(system('which git'), '\n\+$', '', '')
-    ]])
+        let g:gitgutter_git_executable = substitute(system('which git'), '\n\+$', '', '')
+      ]])
     end,
   },
   {
     'rhysd/conflict-marker.vim',
     config = function()
       vim.cmd([[
-    let g:conflict_marker_enable_mappings = 0
-    ]])
+        let g:conflict_marker_enable_mappings = 0
+      ]])
     end,
   },
 }
