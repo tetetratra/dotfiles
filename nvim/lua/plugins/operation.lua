ed3c85c1661ae3556bac90bd1859f50e3bd67fb5
tetetratra <44367208+tetetratra@users.noreply.github.com>
2025-06-09 00:49:07 +0900

    下の方にfilenameを表示するように

--- nvim/lua/plugins/operation.lua
+++ nvim/lua/plugins/operation.lua
@@ -52,6 +52,7 @@ return {
         sections = {
           lualine_a = {},
           lualine_b = {},
+          lualine_c = {{ 'filename', path = 1 }},
           lualine_x = {},
           lualine_y = {},
           lualine_z = {},
@@ -59,6 +60,7 @@ return {
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
+          lualine_c = {{ 'filename', path = 1 }},
           lualine_x = {},
           lualine_y = {},
           lualine_z = {},
