e13347e4aa91cf50c9befb8fd347718e51bf8aa0
tetetratra <44367208+tetetratra@users.noreply.github.com>
2026-08-17 17:23:04 +0900

    補完候補でTabが機能するよう設定を修正

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,16 +2,18 @@
 return {
   {
     "github/copilot.vim",
+    init = function()
+      vim.g.copilot_no_tab_map = true
+    end,
     config = function()
       vim.api.nvim_set_keymap("i", "<C-f>", "<Plug>(copilot-next)", { silent = true })
-      vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept('<Right>')", { noremap = true, silent = true, expr = true })
+      vim.api.nvim_set_keymap("i", "<C-l>", [[copilot#Accept("\<Right>")]], { noremap = true, silent = true, expr = true })
       vim.api.nvim_set_keymap(
         "i",
         "<Right>",
-        "copilot#Accept('<Right>')",
+        [[copilot#Accept("\<Right>")]],
         { noremap = true, silent = true, expr = true }
       )
-      vim.g.copilot_no_tab_map = true
     end,
   },
 }
--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -13,11 +13,11 @@ return {
         return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
       end
       local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
-      keyset('i', '<TAB>', [[coc#pum#visible() ? coc#pum#next(1) : (v:lua.check_back_space() ? '<TAB>' : coc#refresh())]], opts)
-      keyset('i', '<Down>', [[coc#pum#visible() ? coc#pum#next(1) : '<Down>']], opts)
-      keyset('i', '<S-TAB>', [[coc#pum#visible() ? coc#pum#prev(1) : '\<C-h>']], opts)
-      keyset('i', '<Up>', [[coc#pum#visible() ? coc#pum#prev(1) : '<Up>']], opts)
-      keyset('i', '<cr>', [[coc#pum#visible() ? coc#pum#confirm() : '\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>']], opts)
+      keyset('i', '<TAB>', [[coc#pum#visible() ? coc#pum#next(1) : (v:lua.check_back_space() ? "<TAB>" : coc#refresh())]], opts)
+      keyset('i', '<Down>', [[coc#pum#visible() ? coc#pum#next(1) : "\<Down>"]], opts)
+      keyset('i', '<S-TAB>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+      keyset('i', '<Up>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<Up>"]], opts)
+      keyset('i', '<cr>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
       keyset('n', '<Space>[', '<Plug>(coc-references)', { silent = true })
       vim.api.nvim_set_keymap('n', '<C-]>', ':lua jump_gf_lsp_tag()<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua show_documentation()<CR>', { noremap = true, silent = true })
